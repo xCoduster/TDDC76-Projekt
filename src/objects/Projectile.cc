6fe939b1c994ee2eff1329f464de85f399a5dab1
turva417 <turva417@tlvm-4-2-1.ad.liu.se>
2023-11-15 19:46:30 +0100

    Lade till skal för Projectile

---
+++ src/objects/Projectile.cc
@@ -0,0 +1,16 @@
+#include "Projectile.h"
+
+void Projectile::update(const sf::Time& dt)
+{
+
+}
+
+void Projectile::movement(const sf::Time& dt)
+{
+
+}
+
+void Projectile::Collision(const Collidable& other)
+{
+	
+}
---
+++ src/objects/Projectile.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include "MovingObject.h"
+
+class Projectile : public MovingObject
+{
+public:
+    Projectile() {}
+
+    virtual void update(const sf::Time& dt) override;
+    virtual void movement(const sf::Time& dt) override;
+    virtual void Collision(const Collidable& other) override;
+    bool playerProjectile;
+    int damage;
+
+};

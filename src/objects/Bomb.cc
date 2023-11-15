0488ce7aadd3cb709e655281b891f7a7502a4643
erisj991 <erisj991@egypten1-302.ad.liu.se>
2023-11-15 17:05:13 +0100

    Bomb har lagts till!

---
+++ src/objects/Bomb.cc
@@ -0,0 +1,28 @@
+#include "Bomb.h"
+
+#include <iostream>
+
+
+Bomb::Bomb()
+{
+	m_Texture.loadFromFile("res/tmp.png");
+	m_Sprite.setTexture(m_Texture);
+}
+
+void Bomb::update(const sf::Time& dt)
+{
+    movement(dt);
+}
+
+void Bomb::movement(const sf::Time& dt)
+{
+    m_Speed.x = 0.0f;
+	m_Speed.y = 0.0f;
+
+	
+}
+
+void Bomb::Collision(const Collidable& other)
+{
+	std::cout << "Collison " << std::endl;
+}

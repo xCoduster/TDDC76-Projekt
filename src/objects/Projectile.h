97b85f1a4561e01438ff265c9d8ff66aadb4b1b9
vicsn047 <vicsn047@su02-104.ad.liu.se>
2023-11-24 11:08:08 +0100

    la till en bool för att bästema om projektilen är fientlig, Ture & victor

--- src/objects/Projectile.h
+++ src/objects/Projectile.h
@@ -5,9 +5,10 @@
 class Projectile : public MovingObject
 {
 public:
-    Projectile(sf::Vector2f cord);
+    Projectile(sf::Vector2f cord, bool isenemy);
     virtual void update(const sf::Time& dt, std::vector<Object*>& new_objects) override;
     virtual void Collision(const Collidable* other, std::vector<Object*>& new_objects) override;
     virtual void movement(const sf::Time& dt) override;
     int damage;
+    bool isenemy;
 };
